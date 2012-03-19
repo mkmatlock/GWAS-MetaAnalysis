fd4c7d64b9e6b469f3c3d8416876c0457fe9447a
Matt Matlock <matt.matlock@gmail.com>
2012-03-18 21:38:02 -0500

    Fixed a problem with run-all.bat (wrong studies excluded)

--- run-all.bat
+++ run-all.bat
@@ -3,7 +3,7 @@ python src/metaAnalysis.py --inc-map --verify --update --exclude-traits data\gwa
 python src/metaAnalysis.py --inc-map --verify --update --exclude-traits data\gwas\gwas_trait_exclude.txt --rm-pseudo --output-dir "html_partial" --freq 2 
 
 python src/metaAnalysis.py --inc-map --verify --update --exclude-traits data\gwas\gwas_trait_exclude.txt --rm-pseudo --output-dir "html_bustamente" --skip-lists --rm-study "2,3,4,5"
-python src/metaAnalysis.py --inc-map --verify --update --exclude-traits data\gwas\gwas_trait_exclude.txt --rm-pseudo --output-dir "html_vamathevan" --skip-lists --rm-study "2,3,4,5"
-python src/metaAnalysis.py --inc-map --verify --update --exclude-traits data\gwas\gwas_trait_exclude.txt --rm-pseudo --output-dir "html_kosiol" --skip-lists --rm-study "2,3,4,5"
-python src/metaAnalysis.py --inc-map --verify --update --exclude-traits data\gwas\gwas_trait_exclude.txt --rm-pseudo --output-dir "html_bakewell" --skip-lists --rm-study "2,3,4,5"
-python src/metaAnalysis.py --inc-map --verify --update --exclude-traits data\gwas\gwas_trait_exclude.txt --rm-pseudo --output-dir "html_nielsen" --skip-lists --rm-study "2,3,4,5"
+python src/metaAnalysis.py --inc-map --verify --update --exclude-traits data\gwas\gwas_trait_exclude.txt --rm-pseudo --output-dir "html_vamathevan" --skip-lists --rm-study "1,3,4,5"
+python src/metaAnalysis.py --inc-map --verify --update --exclude-traits data\gwas\gwas_trait_exclude.txt --rm-pseudo --output-dir "html_kosiol" --skip-lists --rm-study "1,2,4,5"
+python src/metaAnalysis.py --inc-map --verify --update --exclude-traits data\gwas\gwas_trait_exclude.txt --rm-pseudo --output-dir "html_bakewell" --skip-lists --rm-study "1,2,3,5"
+python src/metaAnalysis.py --inc-map --verify --update --exclude-traits data\gwas\gwas_trait_exclude.txt --rm-pseudo --output-dir "html_nielsen" --skip-lists --rm-study "1,2,3,4"
