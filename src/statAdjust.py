0b83d87cc6d503e8e2e7a151200615b808eb1d9b
Matt Matlock <matt.matlock@gmail.com>
2012-07-15 17:45:26 -0500

    FDR computes multiple simultaneous significance levels --Matlock

--- src/statAdjust.py
+++ src/statAdjust.py
@@ -5,27 +5,26 @@ def sort(test_results):
 def prune(significance, test_results):
     return [pair for pair in test_results if pair[1] <= significance]
 
-def benjamini(significance, test_results):
-    pruned_results = prune(significance, test_results)
-    sorted_results = sort(pruned_results)
+def bonferoni(significance, test_results):
+    sorted_results = sort(test_results)
     x = len(sorted_results)
     return [(pair[0], pair[1] * (x - i)) for i, pair in enumerate(sorted_results)]
-    
-def falseDiscoveryRate(significance, test_results, permutation_results):
-    t_categories = set([pair[0] for pair in prune(significance, test_results)])
+
+
+def falseDiscoveryRate(significance_levels, permutation_results, N_h):
+    return [ __internal_falseDiscoveryRate(significance, permutation_results, N_h) for significance in significance_levels ]
+
+def __internal_falseDiscoveryRate(significance, permutation_results, N_h):
     f_categories = set([pair[0] for pair in prune(significance, permutation_results)])
 
-    if( len(t_categories) == 0 ):
-        return 0.0
-    
-    return float(len(f_categories)) / float(len(t_categories))
+    return float(len(f_categories)) / float(N_h)
 
 if __name__ == "__main__":
     sig = 0.005
     testset = [('a', 0.003),('b', 0.0006),('c', 0.001),('d', 0.01),('e', 0.0004)] 
     pruned = prune(sig, testset)
-    
-    print sort(pruned)
-    print benjamini(sig, testset)
 
-    print falseDiscoveryRate(sig, testset, [('a', 0.001), ('b', 0.04), ('c', 0.1), ('d', 0.76), ('e', 0.002)])
+    print sort(pruned)
+    print bonferoni(sig, testset)
+
+    print falseDiscoveryRate([0.05, sig, 0.0005], [('a', 0.001), ('b', 0.04), ('c', 0.1), ('d', 0.76), ('e', 0.002)], len(testset))
